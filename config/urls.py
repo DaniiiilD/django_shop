a66aaf4f5fa5b7c422c5fa418834e1ce162df390
DaniiiilD <daniil_code@example.com>
2026-07-10 14:28:58 +0300

    Add users API with roles and JWT authentication

--- config/urls.py
+++ config/urls.py
@@ -19,11 +19,20 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/catalog/', include('catalog.urls')),
     path('api/orders/', include('orders.urls')),
+    path('api/users/', include('users.urls')),
+    
+    path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    
     path('api/schema/', SpectacularAPIView.as_view(), name ='schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger_ui'),
 ]
